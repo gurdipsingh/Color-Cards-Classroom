b52c05e7a12fca2f7bac9fff6029a5eeea9e5392
Nosiaj <jaisonpalakat@hotmail.com>
2020-12-16 15:57:01 +0100

    Add selfmade Timer class

---
+++ lib/components/timeCounter.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+
+class TimeCounter extends StatefulWidget{
+  final onTimeUp;
+
+  TimeCounter({
+    Key key,
+    this.onTimeUp
+  }) : super(key: key);
+
+  @override
+  _TimeCounter createState() => _TimeCounter();
+}
+
+class _TimeCounter extends State<TimeCounter> {
+
+  int counter = 10;
+  Timer timer ;
+
+
+  void startTime(context){
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if(counter >0){
+          counter --;
+        }
+        else{
+          timer.cancel();
+          widget.onTimeUp();
+        }
+      });
+    });
+  }
+
+  void initState(){
+    startTime(context);
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(this.counter.toString())
+      ],
+    );
+  }
+
+}
